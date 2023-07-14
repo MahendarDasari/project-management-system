88fa9e13d68bede1c8f98301cd126eaf380bc45f
Mahendar DASARI <mahendar.dasari@hexagon.com>
2023-07-14 19:07:58 +0530

    Added filter

--- FrondEnd/my-app/src/components/ProductList.tsx
+++ FrondEnd/my-app/src/components/ProductList.tsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/types';
-
-import { fetchProducts, addProduct, updateProduct, deleteProduct } from '../redux/actions/productActions';
+import {
+  fetchProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+} from '../redux/actions/productActions';
 import { Product, NewProduct } from '../redux/types';
 import ProductComponent from './Product';
-
-import { Table, TableHead, TableRow, TableCell, TableBody, TextField, Button } from '@mui/material';
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  TextField,
+  Button,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+} from '@mui/material';
 import { styled } from '@mui/system';
-
-import './ProductList.css'; // Import the CSS file
-import { blue, orange } from '@mui/material/colors';
-import ProductAddComponent from './ProductAddComponent';
+import './ProductList.css';
 import { fetchCategories } from '../redux/actions/categoryActions';
 
-// Define custom styles using makeStyles
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   fontWeight: 'bold',
   backgroundColor: '#f5f5f5',
@@ -25,7 +36,7 @@ const StyledTable = styled(Table)({
 });
 
 const StyledTableBody = styled(TableBody)({
-  height: '400px', // Adjust the height as needed
+  height: '400px',
   overflowY: 'auto',
 });
 
@@ -35,81 +46,134 @@ const StyledTableHead = styled(TableHead)({
   backgroundColor: '#f5f5f5',
 });
 
-
 const ProductList: React.FC = () => {
   const dispatch = useDispatch();
   const products = useSelector((state: RootState) => state.products);
   const categories = useSelector((state: RootState) => state.categories);
-  useEffect(() => {
-    // Fetch the categories on component mount
-    dispatch(fetchCategories());
-  }, [dispatch]);
-  
+
   const [showModal, setShowModal] = useState(false);
+  const [categoryFilter, setCategoryFilter] = useState<string>('');
+  const [subcategoryFilter, setSubcategoryFilter] = useState<string>('');
+
   useEffect(() => {
-    // Fetch the products on component mount
+    dispatch(fetchCategories());
     dispatch(fetchProducts());
   }, [dispatch]);
 
   const handleAddProduct = (newProduct: NewProduct) => {
-    // Handle add product logic
     dispatch(addProduct(newProduct));
   };
 
   const handleUpdateProduct = (updatedProduct: Product) => {
-    // Handle update product logic
-    console.log("updatedProduct",updatedProduct);
     dispatch(updateProduct(updatedProduct));
   };
 
   const handleDeleteProduct = (productId: string) => {
-    // Handle delete product logic
     if (window.confirm('Are you sure you want to delete this product?')) {
       dispatch(deleteProduct(productId));
     }
   };
 
-  // Apply custom styles
+  const handleCategoryChange = (
+    e: React.ChangeEvent<{ value: unknown }>
+  ) => {
+    setCategoryFilter(e.target.value as string);
+    setSubcategoryFilter('All');
+  };
+
+  const handleSubcategoryChange = (
+    e: React.ChangeEvent<{ value: unknown }>
+  ) => {
+    setSubcategoryFilter(e.target.value as string);
+  };
+
+  const filteredProducts = products.filter((product) => {
+    if (categoryFilter && subcategoryFilter && subcategoryFilter !== "All" && categoryFilter !== "All") {
+      return (
+        product.categoryId === parseInt(categoryFilter) &&
+        product.subcategoryId === parseInt(subcategoryFilter)
+      );
+    } else if (categoryFilter &&  categoryFilter !== "All") {
+      return product.categoryId === parseInt(categoryFilter);
+    } else if (subcategoryFilter && subcategoryFilter !== "All" ) {
+      return product.subcategoryId === parseInt(subcategoryFilter);
+    } else {
+      return true;
+    }
+  });
+
   return (
     <div className="product-list-container">
-    <div className="add-product-container">
-    <Button variant="contained" onClick={() => setShowModal(true)}>
-      Add Product
-    </Button>
-  </div>
-    <StyledTable>
-      <StyledTableHead>
-        <TableRow>
-        <StyledTableCell >Code</StyledTableCell>
-          <StyledTableCell >Name</StyledTableCell>
-          <StyledTableCell>Quantity</StyledTableCell>
-          <StyledTableCell>Price</StyledTableCell>
-          <StyledTableCell>Description</StyledTableCell>
-          <StyledTableCell>Actions</StyledTableCell>  
-        </TableRow> 
-      </StyledTableHead>
-      <StyledTableBody>
-        {products.map((product) => (
-          <ProductComponent
+      <div className="filter-container">
+        <FormControl>
+          <InputLabel>Category</InputLabel>
+          <Select
+            value={categoryFilter}
+            onChange={handleCategoryChange as any}
+          >
+            <MenuItem value="All">All Categories</MenuItem>
+            {categories.map((category) => (
+              <MenuItem key={category.id} value={category.id}>
+                {category.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        {(categoryFilter !== "All") && (
+          <FormControl>
+            <InputLabel>Subcategory</InputLabel>
+            <Select
+              value={subcategoryFilter}
+              onChange={handleSubcategoryChange as any}
+            >
+              <MenuItem value="All">All Subcategories</MenuItem>
+              {categories
+                .find((category) => category.id === parseInt(categoryFilter))
+                ?.subcategories.map((subcategory) => (
+                  <MenuItem
+                    key={subcategory.id}
+                    value={subcategory.id}
+                  >
+                    {subcategory.name}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        )}
+      </div>
+
+      <StyledTable>
+        <StyledTableHead>
+          <TableRow>
+            <StyledTableCell>Code</StyledTableCell>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell>Quantity</StyledTableCell>
+            <StyledTableCell>Price</StyledTableCell>
+            <StyledTableCell>Description</StyledTableCell>
+            <StyledTableCell>Actions</StyledTableCell>
+          </TableRow>
+        </StyledTableHead>
+        <StyledTableBody>
+          {filteredProducts.map((product) => (
+            <ProductComponent
               key={product.id}
               product={product}
               onUpdate={handleUpdateProduct}
               onDelete={handleDeleteProduct}
               categories={categories}
-            />)
-    )} 
-      </StyledTableBody>    
-    </StyledTable>
+            />
+          ))}
+        </StyledTableBody>
+      </StyledTable>
 
-  
+      <Button
+        className="add-product-button"
+        variant="contained"
+        onClick={() => setShowModal(true)}
+      >
+        Add Product
+      </Button>
 
-      {/* Render the add product modal */}
-      <ProductAddComponent
-        open={showModal}
-        onClose={() => setShowModal(false)}
-        onAdd={handleAddProduct}
-        categories={categories}
-      />
     </div>
   );
 };
